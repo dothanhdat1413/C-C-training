9dec56d8782383ddced660fbe6b79387086f7561
dothanhdat1413 <datdo2525@gmail.com>
2024-05-26 11:29:45 +0700

    update

--- KTLT-HUST/9.5.24_Class_continue/Array/Array.cpp
+++ KTLT-HUST/9.5.24_Class_continue/Array/Array.cpp
@@ -59,6 +59,11 @@ class Array {
             a[i] = x;
         }
 
+        friend ostream& operator<<(ostream& out, const Array& arr);
+        friend istream& operator>>(istream& in, Array& arr);
+        friend int operator[](int i, const Array& arr) { 
+           return arr.a[i]; 
+        }
         /**
          * @brief Hàm xóa mảng đã cấp phát
          * 
@@ -66,17 +71,39 @@ class Array {
         ~Array() {
             delete[] a;
         }
+
+        
 };
+
+ostream& operator<<(ostream& out, const Array& arr) {
+    cout<<endl;
+    for (int i = 0; i < arr.n; i++) {
+        out << arr.a[i] << " ";
+    }
+    cout<<endl;
+    return out;
+}
+
+istream& operator>>(istream& in, Array& arr) {
+    delete[] arr.a;
+    cout<<"Nhap vao so phan tu mang: ";
+    in>> arr.n;
+    arr.a = new int[arr.n];
+    
+    for(int i = 0; i < arr.n; i++) {
+        std::cout<<"\nNhap vao phan tu mang thu "<<i+1<<": ";
+        in >> arr.a[i];
+    }
+    return in;
+}
+
+
 int main() {
     Array a(4,5);
     a.print();
     a.set(1,6);
     a.get_i(1);
 
-    Array a1(4,5);
-    Array a2(a1);
-    a2.set(1,6);
-    cout<<endl;
-    a1.get_i(1);
-    a2.get_i(1);
+
+    cout<<a;
 }
